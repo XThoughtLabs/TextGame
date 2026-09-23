f809baa4501f7ac0d9b0f8523c85bf6d2c276f9e
Vlad Braiman <321729460+XThoughtLabs@users.noreply.github.com>
2026-09-23 11:13:23 +0300

    test: add Unreal C++ module smoke test

--- game/Source/TextGame/Private/TextGame.cpp
+++ game/Source/TextGame/Private/TextGame.cpp
@@ -2,4 +2,19 @@
 
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, TextGame, "TextGame");
+class FTextGameModule final : public FDefaultGameModuleImpl
+{
+public:
+    virtual void StartupModule() override
+    {
+        FDefaultGameModuleImpl::StartupModule();
+
+        UE_LOG(
+            LogTemp,
+            Display,
+            TEXT("TextGame C++ smoke test: module loaded successfully.")
+        );
+    }
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FTextGameModule, TextGame, "TextGame");
