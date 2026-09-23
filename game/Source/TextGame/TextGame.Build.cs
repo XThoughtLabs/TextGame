d68929d494450da0ea1766677651ad2cfe5e99cc
Vlad Braiman <321729460+XThoughtLabs@users.noreply.github.com>
2026-09-23 11:00:27 +0300

    feat: initialize Unreal Engine C++ project

---
+++ game/Source/TextGame/TextGame.Build.cs
@@ -0,0 +1,18 @@
+using UnrealBuildTool;
+
+public class TextGame : ModuleRules
+{
+    public TextGame(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine"
+            }
+        );
+    }
+}
